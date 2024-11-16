e95f09fd636525acf7c37fa2a506d6f393acf737
XShawn-0703 <sino167200333@gmail.com>
2024-11-16 15:08:58 +0700

    test

---
+++ b1.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  //khai bao bien co ban
+  var ten = ' Sang'; //tu suy luan
+  int tuoi = 21; //khai bao ro rang
+
+  // su dung null safety
+  String? tenNullable;
+  tenNullable = null;
+
+  // Su dung bien late
+  late String moTa;
+  moTa = 'Lap trinh Dart';
+
+  // Su dung final va const
+  final String quocGia = 'VietNam';
+  const int nam = 2024;
+
+  //In gia tri bien
+  print('Ten: $ten');
+  print('Tuoi: $tuoi');
+  print('Mo ta: $moTa');
+  print('Quoc Gia: $quocGia');
+  print('Nam: $nam');
+
+  //Kiem tra bien nullable
+  int? soLuong1 = 5;
+  assert(soLuong1 == null);
+
+  //Kiem tra bien nullable
+  int? soLuong;
+  assert(soLuong == null);
+  print(soLuong);
+}
